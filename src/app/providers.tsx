7eeaea9a59fd94cf696101a048bfed96937e24f8
Illia Osmanov <me@osmanov.dev>
2025-10-24 14:43:09 +0300

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,31 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
 import FrameProvider from "~/components/providers/FrameProvider";
 
-
-const WagmiProvider = dynamic(
-  () => import("~/components/providers/WagmiProvider"),
-  {
-    ssr: false,
-  }
-);
-
-const ErudaProvider = dynamic(
-  () => import("~/components/providers/ErudaProvider"),
-  {
-    ssr: false,
-  }
-);
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <WagmiProvider>
-      <FrameProvider>
-        <ErudaProvider />
-        {children}
-      </FrameProvider>
-    </WagmiProvider>
-  );
+  return <FrameProvider>{children}</FrameProvider>;
 }
